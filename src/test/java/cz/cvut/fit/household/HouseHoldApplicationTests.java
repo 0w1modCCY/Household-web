b36c925937f05a958dd630adeeb098dd7eaa13b3
yakunole <yakunole@fit.cvut.cz>
2022-04-01 22:16:52 +0200

    Project template created

---
+++ src/test/java/cz/cvut/fit/household/HouseHoldApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.household;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseHoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
