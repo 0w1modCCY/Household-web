afc266995f4b6a978a72fe339be3499f928cc2a7
Victor Moreno <vmahhview10@gmail.com>
2022-05-09 04:13:04 +0200

    Sidebar added

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -13,6 +13,14 @@ function remcl(){
 	}
 }
 
+function w3_open() {
+	document.getElementById("sidebar").style.display = "block";
+}
+
+function w3_close() {
+	document.getElementById("sidebar").style.display = "none";
+}
+
 
 inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
